8d3d9e86e37a1909bd32802575596c4a556b4deb
Alisha Khan <alisha.khan.mridula@gmail.com>
2023-05-21 10:38:45 +0600

    Version 1

--- Monthly Expense Bill Project V1/Models/Expense.py
+++ Monthly Expense Bill Project V1/Models/Expense.py
@@ -10,21 +10,35 @@ class Expense(Base,BaseModel):
     EmployeeID = Column(String(20), unique= True, nullable=False)
     ExpenseMonth = Column(DateTime())
     CheckedByID = Column (String(10))
-    CheckedOn = Column(DateTime())
+    CheckedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    
     VerifiedByID = Column(String(10), nullable=True)
-    VerifiedOn = Column(DateTime())
+    VerifiedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    
     ForwardedByID = Column(String(10), nullable=True)
-    ForwardedOn = Column(DateTime())
+    ForwardedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    
     RecommendedByID = Column(String(10), nullable=True)
-    RecommendedOn = Column(DateTime())
+    RecommendedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    
     ApprovedByID = Column(String(10), nullable=True)
-    ApprovedOn = Column(DateTime())
+    ApprovedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    
     FCAApprovedByID = Column(String(10), nullable=True)
-    FCAApprovedOn = Column(DateTime())
+    FCAApprovedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    
     FAApprovedByID = Column(String(10), nullable=True)
-    FAApprovedOn = Column(DateTime())
+    FAApprovedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    
     ManagementApprovedByID = Column(String(10),nullable=True)
-    ManagementApprovedOn = Column(DateTime())
+    ManagementApprovedOn = Column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
     
     
 class ExpenseSchema(Schema):
